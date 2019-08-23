932ae3f0b1a767704e6f8576ee3a5a64e1052ea6
Raiyan <raiyan.sharif.1234@gmail.com>
2019-08-24 02:10:45 +0600

    Designing Card model

---
+++ Concentration/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by raiyan sharif on 18/8/19.
+//  Copyright © 2019 raiyan sharif. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by raiyan sharif on 18/8/19.
+//  Copyright © 2019 raiyan sharif. All rights reserved.
+//
+
+import Foundation
+
+//class Concentration{
+//    var cards: Array<Card>
+//    
+//    func chooseCard(at index: Int){
+//        
+//    }
+//}
